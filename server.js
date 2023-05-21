c9f74465325814f82604c43dc8ab7ecc8af27cea
wren <zennguyen2511@gmail.com>
2023-05-21 17:38:53 -0500

    rename dashboard, adding code in dash board

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+const session = require('express-session');
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-require('./routes/api/homepage')(app);
-require('./routes/api/blogpost')(app);
-require('./routes/api/login')(app);
+// Configure session middleware
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+// Import and configure the home module
+const homeModule = require('./routes/api/homepage');
+homeModule(app);
+// Import and configure the login module
+const loginModule = require('./routes/api/login');
+loginModule(app);
+
+// Import and configure the dashboard module
+const dashboardModule = require('./routes/api/dashboard');
+dashboardModule(app);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
