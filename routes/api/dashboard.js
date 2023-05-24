60eb9f569e8f32e340649847719a39a383213d74
wren <zennguyen2511@gmail.com>
2023-05-23 21:22:49 -0500

    update style for home,dashboard

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -45,10 +45,41 @@ app.get('/dashboard', (req, res) => {
           <style>
           body {
             font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #184d47; /* Neo green background color */
-            color: #e9e9e9; /* Font color opposite to background */
+          }
+          .container {
+            max-width: auto;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f2f2f2;
+          }
+          h1 {
+            text-align: center;
+          }
+          label {
+            display: block;
+            margin-bottom: 10px;
+          }
+          input[type="text"],
+          input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+          }
+          button {
+            width: 100%;
+            padding: 10px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+          }
+          button:hover {
+            background-color: #45a049;
           }
           .top-bar {
             display: flex;
@@ -64,29 +95,6 @@ app.get('/dashboard', (req, res) => {
             margin-right: 10px;
             background-color: #520000; /* Dark brown red background color */
           }
-          h1 {
-            color: #520000; /* Dark brown red font color */
-            background-color: aquamarine; /* Aquamarine background color */
-           
-          }
-          .blog-post {
-            background-color: aquamarine; /* Aquamarine background color */
-            color: #520000; /* Dark brown red font color */
-            padding: 10px;
-            margin-bottom: 20px;
-          }
-
-          h2, p, span {
-            margin: 0;
-            padding: 0;
-          }
-          h2 {
-            display: inline-block;
-          }
-          h2, p {
-            background-color: aquamarine; /* Aquamarine background color */
-            color: #520000; /* Dark brown red font color */
-          } 
           </style>
         </head>
         <body>
--- routes/api/homepage.js
+++ routes/api/homepage.js
@@ -8,71 +8,58 @@ app.get('/', (req, res) => {
         <head>
           <title>The Tech Blog</title>
           <style>
-            body {
-              font-family: Arial, sans-serif;
-              margin: 0;
-              padding: 0;
-              background-color: #184d47; /* Neo green background color */
-              color: #e9e9e9; /* Font color opposite to background */
-            }
-            
-            .container {
-              max-width: 800px;
-              margin: 0 auto;
-              padding: 20px;
-              background-color: #fff;
-              box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-            }
-            
-            h1 {
-              color: #520000; /* Dark brown red font color */
-              background-color: aquamarine; /* Aquamarine background color */
-              padding: 10px;
-              margin-bottom: 20px;
-              display: inline-block;
-            }
-            
-            h2, p {
-              background-color: aquamarine; /* Aquamarine background color */
-              color: #520000; /* Dark brown red font color */
-              padding: 10px;
-              margin-bottom: 20px;
-            }
-            
-            table {
-              width: 100%;
-              border-collapse: collapse;
-              margin-top: 20px;
-              box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); /* Shadow applied to table */
-            }
-            
-            th, td {
-              padding: 10px;
-              text-align: left;
-              border-bottom: 1px solid #e9e9e9;
-            }
-            
-            th {
-              background-color: #520000;
-              color: #fff;
-            }
-            tbody{
-                color: black;
-            }
-            .top-bar {
-                display: flex;
-                justify-content: flex-start;
-                align-items: center;
-                margin-bottom: 20px;
-              }
-      
-              .top-bar a {
-                color: #e9e9e9;
-                text-decoration: none;
-                padding: 10px;
-                margin-right: 10px;
-                background-color: #520000; /* Dark brown red background color */
-              }
+          body {
+            font-family: Arial, sans-serif;
+          }
+          .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f2f2f2;
+          }
+          h1 {
+            text-align: center;
+          }
+          label {
+            display: block;
+            margin-bottom: 10px;
+          }
+          input[type="text"],
+          input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+          }
+          button {
+            width: 100%;
+            padding: 10px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+          }
+          button:hover {
+            background-color: #45a049;
+          }
+          .top-bar {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            margin-bottom: 20px;
+          }
+  
+          .top-bar a {
+            color: #e9e9e9;
+            text-decoration: none;
+            padding: 10px;
+            margin-right: 10px;
+            background-color: #520000; /* Dark brown red background color */
+          }
           </style>
         </head>
         <body>
