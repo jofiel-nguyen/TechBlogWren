6780e415d981b3100715b151fee5e58d24175114
wren <zennguyen2511@gmail.com>
2023-05-24 15:59:42 -0500

    create style.css, link to homepage

--- routes/api/homepage.js
+++ routes/api/homepage.js
@@ -1,128 +1,61 @@
+const express = require('express');
+const path = require('path');
+
 module.exports = function(app) {
-// Homepage route
-app.get('/', (req, res) => {
+  // Serve static files from the "public" directory
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/style.css', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/css');
+    next();
+  });
+  // Homepage route
+  app.get('/', (req, res) => {
     // Render the homepage with specified styles
     res.send(`
-
       <html>
         <head>
           <title>The Tech Blog</title>
-          <style>
-          body {
-            font-family: Arial, sans-serif;
-          }
-          .container {
-            max-width: auto;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            background-color: #f2f2f2;
-          }
-          h1 {
-            text-align: center;
-          }
-          label {
-            display: block;
-            margin-bottom: 10px;
-          }
-          input[type="text"],
-          input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-          }
-          button {
-            width: 100%;
-            padding: 10px;
-            background-color: #4CAF50;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-          }
-          button:hover {
-            background-color: #45a049;
-          }
-          .top-bar {
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
-            margin-bottom: 20px;
-          }
-  
-          .top-bar a {
-            color: #e9e9e9;
-            text-decoration: none;
-            padding: 10px;
-            margin-right: 10px;
-            background-color: #520000; /* Dark brown red background color */
-          }
-          .table-style {
-            border-collapse: collapse;
-            width: 100%;
-          }
-          
-          .table-style th, .table-style td {
-            border: 1px solid #ddd;
-            padding: 8px;
-          }
-          
-          .table-style th {
-            background-color: #f2f2f2;
-            font-weight: bold;
-          }
-          
-          .table-style tr:nth-child(even) {
-            background-color: #f9f9f9;
-          }
-          
-          .table-style tr:hover {
-            background-color: #e9e9e9;
-          }
-          
-          </style>
+          <link rel="stylesheet" href="/style.css">
         </head>
         <body>
-        <div class="container">
-        <div class="top-bar">
-          <a href="/">Home</a>
-          <a href="/dashboard">Dashboard</a>
-          <a href="/login">Log in</a>
-        </div>
           <div class="container">
-            <h1>The Tech Blog</h1>
-            <h2>About MVC (Model-View-Controller)</h2>
-            <p>
-              MVC is a software architectural pattern commonly used in web development. It divides an application into three interconnected components:
-            </p>
-            <table class="table-style">
-              <thead>
-                <tr>
-                  <th>Component</th>
-                  <th>Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Model</td>
-                  <td>Represents the data and business logic of the application.</td>
-                </tr>
-                <tr>
-                  <td>View</td>
-                  <td>Handles the presentation and user interface.</td>
-                </tr>
-                <tr>
-                  <td>Controller</td>
-                  <td>Acts as an intermediary between the Model and View, handling user input and updating the Model or View as necessary.</td>
-                </tr>
-              </tbody>
-            </table>
-            <p>
-              By using the MVC pattern, developers can achieve separation of concerns, allowing for easier maintenance, code reusability, and scalability of the application.
-            </p>
+            <div class="top-bar">
+              <a href="/">Home</a>
+              <a href="/dashboard">Dashboard</a>
+              <a href="/login">Log in</a>
+            </div>
+            <div class="container">
+              <h1>The Tech Blog</h1>
+              <h2>About MVC (Model-View-Controller)</h2>
+              <p>
+                MVC is a software architectural pattern commonly used in web development. It divides an application into three interconnected components:
+              </p>
+              <table class="table-style">
+                <thead>
+                  <tr>
+                    <th>Component</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Model</td>
+                    <td>Represents the data and business logic of the application.</td>
+                  </tr>
+                  <tr>
+                    <td>View</td>
+                    <td>Handles the presentation and user interface.</td>
+                  </tr>
+                  <tr>
+                    <td>Controller</td>
+                    <td>Acts as an intermediary between the Model and View, handling user input and updating the Model or View as necessary.</td>
+                  </tr>
+                </tbody>
+              </table>
+              <p>
+                By using the MVC pattern, developers can achieve separation of concerns, allowing for easier maintenance, code reusability, and scalability of the application.
+              </p>
+            </div>
           </div>
         </body>
       </html>
--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
-const app = express();
 const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 // Configure session middleware
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: true
 }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public'), { 
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
+
 // Import and configure the home module
 const homeModule = require('./routes/api/homepage');
 homeModule(app);
+
 // Import and configure the login module
 const loginModule = require('./routes/api/login');
 loginModule(app);
