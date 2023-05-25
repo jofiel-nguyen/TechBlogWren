d4df1bcc6ab6d74fd98c4b464401be790c0e0f27
wren <zennguyen2511@gmail.com>
2023-05-24 20:49:32 -0500

    update, fix bug in edit handle bar

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -1,20 +1,20 @@
+let blogPosts = [
+  {
+    id: 1,
+    title: 'Understanding MVC (Model-View-Controller)',
+    content: 'MVC is a software architectural pattern commonly used in web development. It divides an application into three interconnected components: the Model, the View, and the Controller. The Model represents the data and business logic, the View handles the presentation and user interface, and the Controller acts as an intermediary between the Model and View, handling user input and updating the Model or View as necessary.',
+    dateCreated: '2023-05-20',
+    author: 'Lisa Smith'
+  },
+  {
+    id: 2,
+    title: 'JavaScript Basics',
+    content: 'JaveScript is a high-level, interpreted programming language primarily used for creating interactive and dynamic web content. It was originally developed by Brendan Eich at Netscape Communications in 1995. JavaScript is widely used on the client-side of web development to add interactivity, validate input, manipulate HTML elements, and perform various actions in response to user interactions.',
+    dateCreated: '2023-05-19',
+    author: 'Jane Smith'
+  }
+];
 module.exports = function(app) {
-  let blogPosts = [
-    {
-      id: 1,
-      title: 'Understanding MVC (Model-View-Controller)',
-      content: 'MVC is a software architectural pattern commonly used in web development. It divides an application into three interconnected components: the Model, the View, and the Controller. The Model represents the data and business logic, the View handles the presentation and user interface, and the Controller acts as an intermediary between the Model and View, handling user input and updating the Model or View as necessary.',
-      dateCreated: '2023-05-20',
-      author: 'Lisa Smith'
-    },
-    {
-      id: 2,
-      title: 'JavaScript Basics',
-      content: 'JaveScript is a high-level, interpreted programming language primarily used for creating interactive and dynamic web content. It was originally developed by Brendan Eich at Netscape Communications in 1995. JavaScript is widely used on the client-side of web development to add interactivity, validate input, manipulate HTML elements, and perform various actions in response to user interactions.',
-      dateCreated: '2023-05-19',
-      author: 'Jane Smith'
-    }
-  ];
   const currentDate = new Date().toISOString().split('T')[0];
 
   // Homepage route
@@ -64,40 +64,41 @@ app.post('/dashboard/delete/:id', (req, res) => {
     res.sendStatus(404); // Send a not found response
   }
 });
+/// Route for editing a blog post
+app.get('/dashboard/edit/:id', (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = blogPosts.find(post => post.id === postId);
 
+  if (post) {
+    const username = req.session.user.username;
+    // Replace the author name with the username
+    post.author = username;
+    // Render the edit handlebars template with the updated post data
+    res.render('edit', { post });
+  } else {
+    res.sendStatus(404); // Send a not found response
+  }
+});
 
-
-  // Route for editing a blog post
-  app.get('/dashboard/edit/:id', (req, res) => {
-    const postId = parseInt(req.params.id);
-    const post = blogPosts.find(post => post.id === postId);
-
-    if (post) {
-      const username = req.session.user.username;
-      // Render the edit handlebars template with the post data
-      res.render('edit', { post });
-    } else {
-      res.sendStatus(404); // Send a not found response
-    }
-  });
-
-  // Route for updating a blog post
- // Route for updating a blog post
+// Route for updating a blog post
 app.post('/dashboard/update/:id', (req, res) => {
   const postId = parseInt(req.params.id);
-  // Retrieve the updated title and content from the request body
-  const { title, content } = req.body;
+  // Retrieve the updated title, content, and author from the request body
+  const { title, content, author } = req.body;
 
-  // Find the blog post by ID and update its title and content
-  const post = blogPosts.find((post) => post.id === postId);
+  // Find the blog post by ID and update its title, content, and author
+  const post = blogPosts.find(post => post.id === postId);
   if (post) {
     post.title = title;
     post.content = content;
+    // Update the author only if it has changed in the form
+    if (author !== post.author) {
+      post.author = author;
+    }
     res.redirect('/dashboard');
   } else {
     res.sendStatus(404);
   }
 });
 
-  
 };
