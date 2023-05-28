cc519e46b8da27e1472a14cecec58d6ad549a09e
Wren Ng <wrenng@big-turtle.attlocal.net>
2023-05-28 11:26:30 -0500

    save posts and update function posts

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -1,19 +1,32 @@
-let blogPosts = [
-  {
-    id: 1,
-    title: 'Understanding MVC (Model-View-Controller)',
-    content: 'MVC is a software architectural pattern commonly used in web development. It divides an application into three interconnected components: the Model, the View, and the Controller. The Model represents the data and business logic, the View handles the presentation and user interface, and the Controller acts as an intermediary between the Model and View, handling user input and updating the Model or View as necessary.',
-    dateCreated: '2023-05-20',
-    author: 'Lisa Smith'
-  },
-  {
-    id: 2,
-    title: 'JavaScript Basics',
-    content: 'JaveScript is a high-level, interpreted programming language primarily used for creating interactive and dynamic web content. It was originally developed by Brendan Eich at Netscape Communications in 1995. JavaScript is widely used on the client-side of web development to add interactivity, validate input, manipulate HTML elements, and perform various actions in response to user interactions.',
-    dateCreated: '2023-05-19',
-    author: 'Jane Smith'
+const fs = require('fs');
+
+let blogPosts = [];
+
+// Function to save blogPosts array to posts.json
+function savePostsToJSON(posts) {
+  const jsonContent = JSON.stringify(posts, null, 2);
+  fs.writeFile('posts.json', jsonContent, 'utf8', err => {
+    if (err) {
+      console.error('Error writing to posts.json:', err);
+    } else {
+      console.log('Posts saved to posts.json');
+    }
+  });
+}
+
+// Load blog posts from posts.json
+fs.readFile('posts.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading posts.json:', err);
+  } else {
+    try {
+      blogPosts = JSON.parse(data);
+    } catch (error) {
+      console.error('Error parsing posts.json:', error);
+    }
   }
-];
+});
+
 module.exports = function(app) {
   const currentDate = new Date().toISOString().split('T')[0];
 
@@ -39,32 +52,24 @@ module.exports = function(app) {
     res.render('new');
   });
   
-  // Route for creating a new blog post
-  app.post('/dashboard/new', (req, res) => {
-    const { title, content } = req.body;
-    const newPost = {
-      id: blogPosts.length + 1,
-      title,
-      content,
-      dateCreated: currentDate,
-      author: req.session.user.username,
-    };
-    blogPosts.push(newPost);
-    res.redirect('/dashboard');
-  });
+ // Route for creating a new blog post
+app.post('/dashboard/new', (req, res) => {
+  const { title, content } = req.body;
+  const newPost = {
+    id: blogPosts.length + 1,
+    title,
+    content,
+    dateCreated: currentDate,
+    author: req.session.user.username,
+  };
+  blogPosts.push(newPost);
 
-// Route for deleting a blog post
-app.post('/dashboard/delete/:id', (req, res) => {
-  const postId = parseInt(req.params.id);
-  const index = blogPosts.findIndex(post => post.id === postId);
-  if (index !== -1) {
-    blogPosts.splice(index, 1);
-    res.sendStatus(200); // Send a success response
-  } else {
-    res.sendStatus(404); // Send a not found response
-  }
+  // Save the updated blogPosts array to posts.json
+  savePostsToJSON(blogPosts);
+
+  res.redirect('/dashboard');
 });
-/// Route for editing a blog post
+// Route for editing a blog post
 app.get('/dashboard/edit/:id', (req, res) => {
   const postId = parseInt(req.params.id);
   const post = blogPosts.find(post => post.id === postId);
@@ -75,6 +80,22 @@ app.get('/dashboard/edit/:id', (req, res) => {
     post.author = username;
     // Render the edit handlebars template with the updated post data
     res.render('edit', { post });
+  } else {
+    res.sendStatus(404); // Send a not found response
+  }
+});
+
+// Route for deleting a blog post
+app.post('/dashboard/delete/:id', (req, res) => {
+  const postId = parseInt(req.params.id);
+  const index = blogPosts.findIndex(post => post.id === postId);
+  if (index !== -1) {
+    blogPosts.splice(index, 1);
+
+    // Save the updated blogPosts array to posts.json
+    savePostsToJSON(blogPosts);
+
+    res.sendStatus(200); // Send a success response
   } else {
     res.sendStatus(404); // Send a not found response
   }
@@ -95,6 +116,10 @@ app.post('/dashboard/update/:id', (req, res) => {
     if (author !== post.author) {
       post.author = author;
     }
+
+    // Save the updated blogPosts array to posts.json
+    savePostsToJSON(blogPosts);
+
     res.redirect('/dashboard');
   } else {
     res.sendStatus(404);
