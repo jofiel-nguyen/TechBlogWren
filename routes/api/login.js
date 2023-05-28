837862134204ed46ccc46e825b1c611d095c2817
Wren Ng <wrenng@big-turtle.attlocal.net>
2023-05-28 11:39:04 -0500

    create sign up button

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,5 +1,5 @@
 const users = require('../../config/users.json');
-
+const fs = require('fs');
 module.exports = function(app) {
   // Login route
   app.get('/login', (req, res) => {
@@ -24,5 +24,40 @@ module.exports = function(app) {
       // Render an error message or redirect back to the login page
       res.send('Invalid username or password');
     }
+  });
+   // Sign-up route
+   app.get('/signup', (req, res) => {
+    // Render the sign-up form
+    res.render('signup');
+  });
+
+  // Handle sign-up form submission
+  app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if the username already exists
+    const existingUser = users.find((user) => user.username === username);
+    if (existingUser) {
+      res.send('Username already exists. Please choose a different username.');
+      return;
+    }
+
+    // Create a new user object
+    const newUser = {
+      username,
+      password
+    };
+
+    // Add the new user to the users array
+    users.push(newUser);
+
+    // Save the updated users array to the users.json file
+    fs.writeFile('./config/users.json', JSON.stringify(users), (err) => {
+      if (err) {
+        res.send('An error occurred while saving the user. Please try again.');
+        return;
+      }
+      res.redirect('/login');
+    });
   });
 };
